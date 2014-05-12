3952a2c4e51fc69a9e58fa1a1ae8c7627d74bfaf
Jonathan Trope <jonathan@jonathantrope.com>
2014-05-12 12:35:09 -0400

    adding more functionality to address module

--- address.py
+++ address.py
@@ -5,6 +5,14 @@ class Address(Base):
     def __init__(self):
         self.street_addresses = self.get_file_data("street_addresses.txt")
         self.cities = self.get_file_data("cities.txt")
+        self.states = self.get_file_data("states.txt")
+        self.states_abbr = self.get_file_data("states_abbr.txt")
+
+    def state(self):
+        return random.choice(self.states)
+
+    def state_abbr(self):
+        return random.choice(self.states_abbr)
 
     def city(self):
         return random.choice(self.cities)
@@ -17,3 +25,9 @@ class Address(Base):
 
     def secondary_address(self):
         return "%s %s" % (random.choice(["Apt.", "Suite"]), random.randint(100, 999))
+
+    def latitude(self):
+        return str(random.uniform(1, 181) - 90)
+
+    def longitude(self):
+        return str(random.uniform(1, 361) - 180)
